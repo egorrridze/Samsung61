d23412bd2f590880a0a9e07659e749b2a874110b
Egor <belyaev.eda@gmail.com>
2019-11-02 16:39:46 +0300

    1.7.7
    Узор

--- app/src/main/java/com/example/samsung61/MyView.java
+++ app/src/main/java/com/example/samsung61/MyView.java
@@ -14,8 +14,29 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(100, 100, 800, 860, paint);
+        canvas.drawColor(Color.YELLOW);
+        for (int y = -canvas.getHeight(); y <= canvas.getHeight(); y+=60){
+            paint.setStrokeWidth(2);
+            canvas.drawLine(0, y, canvas.getWidth(), y+canvas.getHeight(), paint);
+        }
+        for (int y = 80; y <= canvas.getHeight(); y+=240){
+            paint.setColor(Color.RED);
+            paint.setStrokeWidth(20);
+            canvas.drawLine(0, y, canvas.getWidth(), y,paint);
+        }
+        for (int y = 200; y <= canvas.getHeight(); y+=240){
+            paint.setColor(Color.BLUE);
+            paint.setStrokeWidth(170);
+            canvas.drawLine(20, y, (canvas.getWidth())/2-30, y,paint);
+            canvas.drawLine((canvas.getWidth())/2+30, y, canvas.getWidth()-30, y,paint);
+        }
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(25);
+        canvas.drawLine(canvas.getWidth()/2, 0, canvas.getWidth()/2, canvas.getHeight(), paint);
+        for (int y = 0; y <= canvas.getHeight(); y+=20){
+            paint.setColor(Color.BLACK);
+            paint.setStrokeWidth(3);
+            canvas.drawLine(canvas.getWidth()/2, y-1, canvas.getWidth()/2, y+1, paint);
+        }
     }
 }
