eae598eb2002c3640a7f6f3937478aa79dce131f
Egor <belyaev.eda@gmail.com>
2019-11-02 20:34:41 +0300

    1.8.9. Модель солнечной системы

--- app/src/main/java/com/example/samsung61/MainActivity.java
+++ app/src/main/java/com/example/samsung61/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyGraph(this));
+        setContentView(new MyView(this));
     }
 }
