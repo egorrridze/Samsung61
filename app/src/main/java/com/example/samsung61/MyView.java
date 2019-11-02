c7ae81846870caea40727ed402f24b5b772ed484
Egor <belyaev.eda@gmail.com>
2019-11-02 20:11:27 +0300

    1.8.7. Простейшая анимация
    Круги летают по экрану

--- app/src/main/java/com/example/samsung61/MyView.java
+++ app/src/main/java/com/example/samsung61/MyView.java
@@ -10,33 +10,41 @@ public class MyView extends View {
     public MyView(Context context) {
         super(context);
     }
+    Paint paint = new Paint();
+    int N = 10; // количество шариков
+    float[] x = new float[N], y = new float[N], vx = new float[N], vy = new float[N];
 
-    @Override
+
+    long lastTime = System.currentTimeMillis();
+    boolean started;
+
     protected void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
-        canvas.drawColor(Color.YELLOW);
-        for (int y = -canvas.getHeight(); y <= canvas.getHeight(); y+=60){
-            paint.setStrokeWidth(2);
-            canvas.drawLine(0, y, canvas.getWidth(), y+canvas.getHeight(), paint);
+        for(int i = 0; i < N; i++){
+            x[i] = (float) (Math.random() * canvas.getWidth());
+            y[i] = (float) (Math.random() * canvas.getHeight());
+            vx[i] = (float) (Math.random());
+            vy[i] = (float) (Math.random());
         }
-        for (int y = 80; y <= canvas.getHeight(); y+=240){
-            paint.setColor(Color.RED);
-            paint.setStrokeWidth(20);
-            canvas.drawLine(0, y, canvas.getWidth(), y,paint);
+        float[] a = new float[N], b = new float[N];
+        for (int i = 0; i < N; i++)
+        if (!started) {
+            // код заполнения массивов
+            started = true;
         }
-        for (int y = 200; y <= canvas.getHeight(); y+=240){
+        for (int i = 0; i < 5; i++) {
+            paint.setColor(Color.MAGENTA);
+            canvas.drawCircle(x[i], y[i], 30, paint);
+        }
+        for (int i = 5; i < N; i++) {
             paint.setColor(Color.BLUE);
-            paint.setStrokeWidth(170);
-            canvas.drawLine(20, y, (canvas.getWidth())/2-30, y,paint);
-            canvas.drawLine((canvas.getWidth())/2+30, y, canvas.getWidth()-30, y,paint);
+            canvas.drawCircle(x[i], y[i], 30, paint);
         }
-        paint.setColor(Color.WHITE);
-        paint.setStrokeWidth(25);
-        canvas.drawLine(canvas.getWidth()/2, 0, canvas.getWidth()/2, canvas.getHeight(), paint);
-        for (int y = 0; y <= canvas.getHeight(); y+=20){
-            paint.setColor(Color.BLACK);
-            paint.setStrokeWidth(3);
-            canvas.drawLine(canvas.getWidth()/2, y-1, canvas.getWidth()/2, y+1, paint);
+        // готовим массивы x и у для следущего кадра
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
         }
+        //запрашиваем перерисовку
+        invalidate();
     }
 }
